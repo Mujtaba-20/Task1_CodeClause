f53ba2aeaae0412a68840d17d3237f47835618ed
Mohd Mujtaba Quadri <quadrimujtaba53@gmail.com>
2026-01-30 23:08:01 +0530

    Update Task1_CodeClause.py

--- Task1_CodeClause.py
+++ Task1_CodeClause.py
@@ -1,127 +1,22 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "id": "48ede2e5-27ed-4a50-b7ab-dd7e28bac659",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# Task 1 : Number Guessing Game"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 11,
-   "id": "eca5d52b-6849-45fe-83f9-534cb5c3ef94",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "====================================================================================================\n",
-      "Number Guessing Game\n",
-      "“Your instincts vs my randomness. Can you crack it?”\n",
-      "====================================================================================================\n"
-     ]
-    },
-    {
-     "name": "stdin",
-     "output_type": "stream",
-     "text": [
-      "Enter your guess:  7\n"
-     ]
-    },
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "Guessed number too low!\n"
-     ]
-    },
-    {
-     "name": "stdin",
-     "output_type": "stream",
-     "text": [
-      "Enter your guess:  43\n"
-     ]
-    },
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "Guessed number too low!\n"
-     ]
-    },
-    {
-     "name": "stdin",
-     "output_type": "stream",
-     "text": [
-      "Enter your guess:  67\n"
-     ]
-    },
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "You guessed it right!\n"
-     ]
-    }
-   ],
-   "source": [
-    "import random\n",
-    "i=1\n",
-    "guess = random.randint(1,99)\n",
-    "print(\"=\"*100)\n",
-    "print(\"Number Guessing Game\")\n",
-    "print(\"“Your instincts vs my randomness. Can you crack it?”\")\n",
-    "print(\"=\"*100)\n",
-    "while i <= 3:\n",
-    "    num = int(input(\"Enter your guess: \"))\n",
-    "    i+=1\n",
-    "    if num == guess:\n",
-    "        print(\"You guessed it right!\")\n",
-    "        break\n",
-    "    elif num > guess:\n",
-    "        print(\"Guessed number too high!\")\n",
-    "    elif num < guess and num > 0:\n",
-    "        print(\"Guessed number too low!\")\n",
-    "    elif num <= 0:\n",
-    "        print(\"Guess a positive number!\")\n",
-    "    \n",
-    "if num != guess:\n",
-    "    print(\"You Lost!\")"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "dbda1433-97ba-4dfd-8649-ba03a311bfeb",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.12.1"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import random
+i=1
+guess = random.randint(1,99)
+print("="*100)
+print("Number Guessing Game")
+print("“Your instincts vs my randomness. Can you crack it?”")
+print("="*100)
+while i <= 3:
+    num = int(input("Enter your guess: "))
+    i+=1
+    if num == guess:
+        print("You guessed it right!")
+        break
+    elif num > guess:
+        print("Guessed number too high!")
+    elif num < guess and num > 0:
+        print("Guessed number too low!")
+    elif num <= 0:
+        print("Guess a positive number!")
+    
+if num != guess:
+    print("You Lost!")
